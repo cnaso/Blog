e2dffd0e63d13818d5580d70e329ee982a1ade0c
Carl Naso <carlnaso@nasomedia.com>
2018-01-21 13:58:25 -0800

    Implemented SidebarController, added Sidebar view model.

--- Blog/Controllers/SidebarController.cs
+++ Blog/Controllers/SidebarController.cs
@@ -1,17 +1,45 @@
-﻿using System;
+﻿using Blog.Core.Domain;
+using Blog.Persistence;
+using Blog.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Blog.Controllers
 {
-    public class LayoutController : Controller
+    public class SidebarController : Controller
     {
+        private BlogContext _context;
+
+        public SidebarController(BlogContext context)
+        {
+            _context = context;
+        }
+
         [ChildActionOnly]
         public ActionResult Sidebar()
         {
-            return PartialView();
+            IList<Tag> tags = _context.Tags.Include(t => t.Posts).ToList();
+            Sidebar sidebar = new Sidebar();
+
+            //TODO: Implement categories
+
+            foreach (var tag in tags)
+            {
+                TagViewModel viewTag = new TagViewModel {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    Slug = tag.Slug,
+                    PostCount = tag.Posts.Count
+                };
+
+                sidebar.Tags.Add(viewTag);
+            } 
+
+            return PartialView("_Sidebar", sidebar);
         }
     }
 }
---
+++ Blog/ViewModels/SidebarViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Blog.ViewModels
+{
+    public class Sidebar
+    {
+        public IList<CategoryViewModel> Categories { get; set; }
+        public IList<TagViewModel> Tags { get; set; }
+
+        public Sidebar()
+        {
+            Categories = new List<CategoryViewModel>();
+            Tags = new List<TagViewModel>();
+        }
+    }
+
+    public class CategoryViewModel
+    {
+        public string Name { get; set; }
+        public int PostCount { get; set; }
+    }
+
+    public class TagViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int PostCount { get; set; }
+    }
+}
